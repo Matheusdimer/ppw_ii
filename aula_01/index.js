22a8369b6a73dc40eaa731b13dddb93188243098
Matheusdimer <matheus-delima@outlook.com>
2021-08-16 21:08:41 -0300

    first commit

---
+++ aula_01/index.js
@@ -0,0 +1,13 @@
+const search = document.getElementById('search');
+const list = document.getElementById('marcas');
+const tableBody = document.querySelector('tbody');
+
+function onInit() {
+
+}
+
+function loadMarcas() {
+
+}
+
+onInit();
