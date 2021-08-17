904c46e002d13f6aa40432274bbfdddb9e860116
Matheus Dimer <70241621+Matheusdimer@users.noreply.github.com>
2021-08-16 21:21:57 -0300

    Carregamento de marcas de carros

--- aula_01/index.js
+++ aula_01/index.js
@@ -2,12 +2,24 @@ const search = document.getElementById('search');
 const list = document.getElementById('marcas');
 const tableBody = document.querySelector('tbody');
 
-function onInit() {
+const PATH = 'https://parallelum.com.br/fipe/api/v1/carros/marcas';
 
+function onInit() {
+    loadMarcas();
 }
 
 function loadMarcas() {
+    fetch(PATH).then(response => response.json()).then(data => renderMarcasList(data));
+}
 
+function renderMarcasList(marcas) {
+    list.append(
+        marcas.map(marca => {
+            const option = document.createElement('option');
+            option.value = marca.nome;
+            return option;
+        })
+    );
 }
 
 onInit();
